7324ee8768c6665b81b70b10a33a56ea6dd42af5
Olesea <emmy.gold@gmail.com>
2018-11-14 21:25:46 +0000

    Split less file into blocks. Fixed catalog-page product iages size (desktop version). Changed gulp file, to process new less files structure and added plugin to add prefixes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,48 @@
+"use strict";
 var gulp = require('gulp'); // Подключаем Gulp
 var less = require('gulp-less'); //Подключаем Less пакет,
-var browserSync = require('browser-sync'); // Подключаем Browser Sync
+var plumber = require("gulp-plumber");
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var browserSync = require("browser-sync").create(); // Подключаем Browser Sync
 
-gulp.task('less', function(){ // Создаем таск less
-  return gulp.src('source/less/**/*.less') // Берем источник
-      .pipe(less()) // Преобразуем Less в CSS посредством gulp-less
-      .pipe(gulp.dest('source/css')) // Выгружаем результата в папку source/css
-      .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+
+gulp.task('style', function(){ // Создаем таск less
+  return gulp.src('source/less/style.less') // Берем источник
+    .pipe(plumber())
+    .pipe(less()) // Преобразуем Less в CSS посредством gulp-less
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('source/css')) // Выгружаем результата в папку source/css
+    .pipe(browserSync.stream()) // Обновляем CSS на странице при изменении
 });
-
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
+/*
+gulp.task('browser-sync', function(){ // Создаем таск browser-sync
   browserSync({ // Выполняем browserSync
       server: { // Определяем параметры сервера
           baseDir: 'source' // Директория для сервера - app
       },
       notify: false // Отключаем уведомления
   });
+});*/
+
+
+gulp.task("watch", function() {
+  browserSync.init({
+    server: { // Определяем параметры сервера
+      baseDir: 'source' // Директория для сервера - app
+    },
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.less", gulp.series("style"));
+  gulp.watch("source/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('watch', ['browser-sync', 'less'], function() {
-  gulp.watch('source/less/**/*.less', ['less']); // Наблюдение за less файлами
-  gulp.watch("source/*.html").on('change', browserSync.reload); // запускаем перезагрузку страницы при изменениях html
-  // Наблюдение за другими типами файлов
-});
+gulp.task("build", gulp.series("style"));
+
 
